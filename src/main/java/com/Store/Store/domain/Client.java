0e461d2a2903988f617a232b5fbe84f97f9094b4
Leonel Enrique Borja Vargas <leonel.borja@sofka.local>
2024-09-20 07:33:47 -0500

    Inicial commit

---
+++ src/main/java/com/Store/Store/domain/Client.java
@@ -0,0 +1,45 @@
+package com.Store.Store.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Table(name = "clients", schema = "store")
+public class Client {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "document_type", nullable = false)
+    private String documentType;
+
+    @Column(name = "document_number", nullable = false)
+    private String documentNumber;
+
+    @Column(name = "email_address", nullable = false)
+    private String emailAddress;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "created_date", nullable = false)
+    private String createdDate;
+
+
+}
